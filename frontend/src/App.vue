7715eb4de127d7d98240e246e498e27de24e2a63
Evan Yang <71720197+Evany226@users.noreply.github.com>
2021-12-15 12:21:52 -0500

    finished setting up
    still need color themes

---
+++ frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap');
+
+#app {
+ font-family: 'Montserrat', sans-serif;
+  text-align: center;
+}
+
+:root {
+/* color themes */
+
+/* main themes */
+
+/* font sizes */
+  --h1: 7.594rem;
+  --h2: 5.063rem;
+  --h3: 3.375rem;
+  --h4: 2.25rem;
+  --h5: 1.5rem;
+}
+
+</style>
