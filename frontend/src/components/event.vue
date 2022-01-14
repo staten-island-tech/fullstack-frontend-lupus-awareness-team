8b77a7a3faea82918400f767ec4de9db82f10a8c
dieuanhn <71412021+dieuanhn@users.noreply.github.com>
2022-01-14 12:26:46 -0500

    lol i dont know

---
+++ frontend/src/components/event.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="event-container">
+    <div class="event-image">
+    <img class="event-image" scr="event-placeholder.jpg">
+    </div>
+    <div class="event-info">
+        <h3 class="event-title">Insert Event Name</h3>
+        <h4 class="event-time">Insert Event Time</h4>
+        <h4 class="event-location">Insert Event Location</h4>
+        <h4 class="event-date">Insert Event Date</h4>
+        <h5 class="event-description">Insert Event Description</h5>
+    </div>
+</div>  
+</template>
+
+<script>
+export default {
+name:"Event",
+}
+</script>
+
+<style>
+</style>
