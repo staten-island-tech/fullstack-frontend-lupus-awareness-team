0f2bd86fc9fd07750f0789341e9d9f4d77b6582f
Evan Yang <71720197+Evany226@users.noreply.github.com>
2022-01-21 12:26:25 -0500

    c

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,31 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app')
+import { domain, clientId, audience } from "../auth_config.json";
+import { Auth0Plugin } from "@/auth/auth0-plugin";
+
+// Install the authentication plugin
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  audience,
+  onRedirectCallback: (appState) => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  },
+});
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
+
+createApp(App).use(store).use(router).mount("#app");
