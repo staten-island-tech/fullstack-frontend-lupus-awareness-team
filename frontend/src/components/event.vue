67ddfdf65763a4a2a0f8f001d8e807f03c5da6cc
dieuanhn <71412021+dieuanhn@users.noreply.github.com>
2022-01-27 12:26:36 -0500

    t swizzle

--- frontend/src/components/event.vue
+++ frontend/src/components/event.vue
@@ -5,11 +5,13 @@
     </div>
     <div class="words">
     <div class="user-info">
+    <div class="circle"></div>
+        <div class="user-profile">
         <h3 class="username">User1234</h3>
         <h4 class="user-type">User Type</h4>
+        </div>
     </div>
     <div class="event-info">
-
         <h3 class="event-title">Insert Event Name</h3>
         <h4 class="event-time">Insert Event Time</h4>
         <h4 class="event-location">Insert Event Location</h4>
@@ -33,6 +35,27 @@ name:"Event",
 </script>
 
 <style>
+.user-info {
+    display: flex;
+    flex-direction: row;
+}
+
+.user-profile {
+    display: flex;
+    flex-direction: column;
+}
+
+.circle {
+  height: 35px;
+  width: 35px;
+  background-color: #555;
+  border-radius: 50%;
+}
+
+.username , .user-type {
+    font-size: 1.5rem;
+    margin: .75rem;
+}
 
 .event-container {
     display: flex;
