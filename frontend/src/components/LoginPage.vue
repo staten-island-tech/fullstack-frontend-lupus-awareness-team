5af02b5941a3399178415405a42c7747bf44f49d
dieuanhn <71412021+dieuanhn@users.noreply.github.com>
2022-02-17 11:44:00 -0500

    lolz

--- frontend/src/components/LoginPage.vue
+++ frontend/src/components/LoginPage.vue
@@ -1,5 +1,27 @@
 <template>
-    <div>
-        
+    <div class="login-page">
+        <div class="login-page-left">
+            <img class="background-image">   
+            <div class="login-info">
+                <h5 class="username"></h5>
+                <h5 class="password"></h5>
+                <button class="login-button"></button>
+            </div>
+        </div>  
+        <div class="login-page-right">
+            <div class="create-account"></div>
+        </div>
     </div>
 </template>
+
+<script>
+export default {
+name:"Login",
+methods: {}
+}
+
+</script>
+
+<style>
+
+</style>
