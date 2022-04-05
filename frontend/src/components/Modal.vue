7a6869051cfac3515fcbf869f293a18932f66850
dieuanhn <71412021+dieuanhn@users.noreply.github.com>
2022-04-05 12:26:52 -0400

    bruh

--- frontend/src/components/Modal.vue
+++ frontend/src/components/Modal.vue
@@ -8,10 +8,40 @@
 
       <section class="modal-body">
         <div class="form-header">
-          <h2 class="title">upload Your Information Here</h2>
-          
+        <h2 class="form-title">Upload Your Information Here</h2>
         </div>
-      </section>
+        <div class="upload-info">
+          <form >
+          <label class="enter-event-name"
+          for="enter-event-name"><b>Enter Event Name</b></label>
+          <input class="event-name" type="text" placeholder="Event Name" name="event-name" required>
+
+          <label class="enter-event-date"
+          for="enter-event-date"><b>Enter Event Date</b></label>
+          <input class="event-date" type="text" placeholder="Event Date" name="event-date" required>
+
+          <label class="enter-event-time"
+          for="enter-event-time"><b>Enter Event Time</b></label>
+          <input class="event-time" type="text" placeholder="Event Time" name="event-time" required>
+
+          <label class="enter-event-address"
+          for="enter-event-address"><b>Enter Event Address</b></label>
+          <input class="event-address" type="text" placeholder="Event Adress" name="event-address" required>
+
+          <label class="enter-event-hours"
+          for="enter-event-hours"><b>Enter Event Hours</b></label>
+          <input class="event-hours" type="text" placeholder="Event Hours" name="event-hours" required>
+
+          <label class="enter-event-tags"
+          for="enter-event-tags"><b>Enter Event Tags</b></label>
+          <input class="event-tags" type="text" placeholder="Event Tags" name="event-tags" required>
+
+          <input type="file" id="myFile" name="filename" class="upload-file-button">
+          <input class="upload-file" type="submit"> 
+
+        </form>
+        </div>
+        </section>
 
     </div>
   </div>
@@ -85,5 +115,31 @@
     background: transparent;
   }
 
+.upload-info {
+  display: flex;
+  flex-direction: column;
+}
+
+.form-title {
+  font-size: 4rem;
+}
+.enter-event-tags, .enter-event-name, .enter-event-time, .enter-event-date, .enter-event-hours, .enter-event-address {
+  font-size: 3rem;
+}
+
+.event-tags, .event-name, .event-time, .event-date, .event-hours, .event-address {
+  margin: 5px 0rem;
+  font-size: 15px;
+  border-style: solid;
+  border-radius: 7px;
+  border-color: black;
+  padding: 3px;
+  width: 40rem;
+} 
+
+form {
+  display: flex;
+  flex-direction: column;
+}
 
 </style>
