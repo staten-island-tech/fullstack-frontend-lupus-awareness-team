d7377b4881b421911ce2cc87f7bb150e1c678b90
ctrlLey <71617500+ctrlLey@users.noreply.github.com>
2022-04-26 12:27:21 -0400

    works!
    just have to put in filling in inputs so the user can change it

--- frontend/src/components/Autocomplete.vue
+++ frontend/src/components/Autocomplete.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="address_complete">
       <div class="search_bar">
+          <!-- can change the @keyup.enter to just @keyup for speedy searches, but that puts out a lottttt of requests and i might hit my monthly quota (30000) -->
         <input @keyup.enter="fetchAddress()" v-model="query" type="text" name="autocomplete" id="search_form" placeholder="Search...">
-        <div class="search_results"></div>
+        <div v-show="searchResults" v-for="(index) in searchResults" :key="index.properties.name" class="search_results">{{index.properties.label}}</div>
         <button class="search_btn">Go</button>
       </div>
       <label for=""></label>
@@ -23,10 +24,11 @@ export default {
     methods: {
     fetchAddress: async function () {
         try {
-        const response = await fetch(`https://api.geocodify.com/v2/autocomplete?api_key=${this.apiKey}&q=${this.query}`);
+        const response = await fetch(`https://api.geocodify.com/v2/autocomplete?api_key=${this.apiKey}&q=${this.query}, USA`);
+        // only searches within america, but can be changed (idk why tho)
         const data = await response.json();
-        this.addressArr = data;
-        console.log(this.addressArr)
+        this.searchResults = data.response.features;
+        console.log(this.searchResults)
       } catch(error) {
           console.log(error)
       }
@@ -44,13 +46,18 @@ export default {
         margin: 1rem auto;
     }
     #search_form {
+        width: 100%;
         font-size: 3rem;
-        width: 50%;
-        height: 5rem;
-        margin: 3rem;
-        padding: 0 2rem;
+        padding: 1rem 2rem;
         border: none;
         outline: none;
+    }
+    .search_bar {
+        margin: 0 auto;
+        width: 60%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
     }
     #search_form:focus {
         border: none;
@@ -59,5 +66,12 @@ export default {
         font-size: 2rem;
         height: 2rem;
         margin: 3;
+    }
+    .search_results {
+        background-color: antiquewhite;
+        width: 100%;
+        font-size: 2.5rem;
+        padding: 1rem 2rem;
+        
     }
 </style>
