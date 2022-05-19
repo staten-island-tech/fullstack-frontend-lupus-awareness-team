5500ba850aa13f3c0d94641633ad5cd874065f3b
Evan Yang <71720197+Evany226@users.noreply.github.com>
2022-05-19 12:01:00 -0400

    c

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -305,6 +305,7 @@ li {
   position: relative;
   box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
     rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+    padding: 0rem 1rem;
 }
 
 .login-container:hover .login-button {
