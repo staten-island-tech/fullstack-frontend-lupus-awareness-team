dd20f6934d4e98574dc90691c5a7b513d2806bf4
J0hns0n-Wang <johnsonwang529@gmail.com>
2022-06-03 18:14:15 -0400

    added redirects file

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,10 +1,11 @@
 import axios from "axios";
+// const token = sessionStorage.getItem('auth-token')
 
 export const HTTP = axios.create({
   baseURL: "http://localhost:3000",
   headers: {
-    Authorization: "Bearer {token}",
-    "Content-Type": "application/json",
+    Authorization: `Bearer {token}`,
+    'Content-type': 'application/json; charset=UTF-8',
   },
   withCredentials: true
 });
