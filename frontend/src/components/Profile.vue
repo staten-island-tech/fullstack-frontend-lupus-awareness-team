a0665f4d55380a2539fcbc089ce3875898f4c4f1
dieuanhn <71412021+dieuanhn@users.noreply.github.com>
2022-06-04 22:21:42 -0400

    bruh

--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="profile_container">
       <div class="profile_image_container">
-        <img class="profile_image" src="../assets/placeholder.jpg">
+      
+      <div class="profile-pic">
+        <label class="-label" for="file">
+          <span class="glyphicon glyphicon-camera"></span>
+          <span>Change Image</span>
+        </label>
+        <input id="file" type="file" @change="loadFile($event)"/>
+        <img src="../assets/placeholder.jpg" id="output" width="200" ref="output" />
+</div>
+
       </div>
+
+
       <div class="profile_text_container">
         <h3 class="profile_name"> {{ this.userArr.userName }}</h3>
         <h4 class="profile_sub"> {{ this.userArr.userEmail }} </h4>
@@ -32,6 +43,12 @@ methods: {
           console.log(error)
       }
     },
+
+  loadFile: function(event) { let image = this.$refs.output; 
+  image.src = URL.createObjectURL(event.target.files[0]);
+}
+
+
 },
 created() {
   this.fetchUser();
@@ -84,5 +101,66 @@ created() {
   font-weight: 500;
   color: var(--profiletext)
 }
+
+
+.profile-pic {
+  color: transparent;
+  transition: all .3s ease;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  transition: all .3s ease;
+}  
+  input {
+    display: none;
+  }
+  
+  img {
+    position: absolute;
+    object-fit: cover;
+    width:165px;
+    height: 165px;
+    box-shadow: 0 0 10px 0 rgba(255,255,255,.35);
+    border-radius: 100px;
+    z-index: 0;
+  }
+  
+  .-label {
+    cursor: pointer;
+    height: 165px;
+    width:165px;
+  }
+  
+  :hover {
+   
+    display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: rgba(0,0,0,.8);
+      z-index: 10000;
+      color: rgb(250,250,250);
+      transition:.2s;
+      border-radius: 100px;
+      margin-bottom: 0;
+  }
+  
+  span {
+    display: inline-flex;
+    padding: .2em;
+    height: 2em;
+  }
+
+body {
+  height: 100vh;
+  background-color: rgb(25, 24, 21);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+} 
+  a:hover {
+    text-decoration: none;
+  }
+
 
 </style>
