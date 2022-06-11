3a4069d69352719c6da249b9795a174d96ddeeff
J0hns0n-Wang <johnsonwang529@gmail.com>
2022-06-10 21:41:21 -0400

    sdc

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const HTTP = axios.create({
-  baseURL: "https://lupus-awareness-backend.onrender.com",
+  baseURL: "http://localhost:3000",
   headers: {
     Authorization: "Bearer {token}",
   },
